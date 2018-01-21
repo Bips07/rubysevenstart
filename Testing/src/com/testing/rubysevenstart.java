2e5fe7f70ed9340ae71fd3e160f5dbea3b6ed1bf
Bips <bipinev@outlook.com>
2018-01-21 21:46:10 +0530

    added main page

--- Testing/src/com/testing/rubysevenstart.java
+++ Testing/src/com/testing/rubysevenstart.java
@@ -1,10 +1,17 @@
 package com.testing;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class rubysevenstart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.setProperty("webdriver.chrome.driver", "C://Users//Bipin//Downloads//Testing//chromedriver_win32//chromedriver.exe");
+		WebDriver d=new ChromeDriver();
+		d.navigate().to("https:google.com");
+		d.close();
 	}
 
 }
