0f57ea37645c7f25a26e2a06052f0c428bf2ddf5
Bips <bipinev@outlook.com>
2018-01-22 00:18:27 +0530

    first test run

--- Testing/src/com/testing/rubysevenstart.java
+++ Testing/src/com/testing/rubysevenstart.java
@@ -2,16 +2,22 @@ package com.testing;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class rubysevenstart {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
+		ChromeOptions co =new ChromeOptions();
+		co.addArguments("--start maximized");
 		System.setProperty("webdriver.chrome.driver", "C://Users//Bipin//Downloads//Testing//chromedriver_win32//chromedriver.exe");
-		WebDriver d=new ChromeDriver();
+		WebDriver d=new ChromeDriver(co);
 		d.navigate().to("https:google.com");
-		d.close();
+		//d.manage().window().maximize();
+		homepage a=new homepage();
+		a.homepag(d);
+		a.login(d);
 	}
 
 }
